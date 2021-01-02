3a4435ca3892e57f19f1c5c2c52c29f3b9481d9d
cslaf <35430865+cslaf@users.noreply.github.com>
2021-01-01 18:33:18 -0600

    Revert "attempt at generic paginator"
    This reverts commit 233b764e78cc6c45bc371ed754b9ed87cca0005e.
    still in the branch for reference

--- Clockwork.Blazor/PagedResultGeneric.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace BlazorPagination
-{
-    public class PagedResultGeneric<T> : PagedResultBase 
-    {
-        public T[] Results { get; set; }
-    }
-}
